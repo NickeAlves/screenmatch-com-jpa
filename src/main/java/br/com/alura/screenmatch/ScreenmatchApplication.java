bf58e06541557833c1b308fee2274ab0e008ec3e
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2024-11-21 19:52:22 -0300

    update to db

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch;
 import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -22,7 +22,7 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
@@ -42,6 +42,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -117,6 +118,7 @@ public class Serie {
                 " | Avaliação : " + avaliacao +
                 " | Atores : " + atores +
                 " | Poster : " + poster +
-                " | Sinopse : " + sinopse;
+                " | Sinopse : " + sinopse +
+                " | Episodios : " + episodios;
     }
 }
